ae25bd658a92155d1f0897a944900a67db1c2a30
Ajith <ajithkumarhari011@gmail.com>
2023-12-28 23:34:34 +0530

    edited environments file

--- frontend/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    siteUrl: "https://dailyhire.ajithkumarhari.co",
-    serverUrl : "https://dailyhire.ajithkumarhari.co/api",
-    googleClientId : "728439674744-g19vlgq565ud4b18eg4oldp0nk5dbt5j.apps.googleusercontent.com",
-    stripeKey: "pk_test_51OFsOJSDlgqaDxgfrx83NzkCWIKqRIeVLjNNHxcU7NrXLEMrddNWCnHHhYmWGB1cGDsvFQvnRvFM80h9aAXLqZ1100AWYv1XL9",
-};
